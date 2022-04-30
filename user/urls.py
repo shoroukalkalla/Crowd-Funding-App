9361e68006f96a4e1cf71d658bdef7cbd8643d08
mohamed159716 <mohamed159716@yahoo.com>
2022-04-30 20:06:47 +0200

    Added Email Verification

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path("signup/", SignUpView.as_view(), name="signup"),
     path("signup/", signup, name="signup"),
     path('home', home, name='home'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-         activate, name='activate'),
+    # path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    #      activate, name='activate'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
 ]
--- user/views.py
+++ user/views.py
@@ -12,12 +12,14 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
+
+from .models import User
 
 
 # class SignUpView(CreateView):
@@ -70,10 +72,10 @@ def home(request):
 
 
 def activate(request, uidb64, token):
-    User = get_user_model()
+    user = User()
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(id=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):

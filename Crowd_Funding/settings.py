58d3096f47ea4dc1f78fe2a3d0e206b310b2638d
mohamed159716 <mohamed159716@yahoo.com>
2022-05-02 14:57:13 +0200

    Created Global Static & Templates Dirs

--- Crowd_Funding/settings.py
+++ Crowd_Funding/settings.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'Crowd_Funding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 'user.templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
 
 
 # media uploading

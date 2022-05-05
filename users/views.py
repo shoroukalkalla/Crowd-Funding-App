e2fa01465e6ef3893d53359e4ecba4d6912b6fd2
Mohamed Shehata <mohamed159716@yahoo.com>
2022-05-06 00:50:20 +0200

    Added Success Email Confirmation Message

--- users/views.py
+++ users/views.py
@@ -132,7 +132,8 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.  <a href="/login"> Login</a>')
+        add_message(request, INFO, 'Thank you for your email confirmation. Now you can login your account')
+        return redirect('/login');
     else:
         return HttpResponse('Activation link is invalid!')
 
